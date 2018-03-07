184fe4fbec7987f2d0c44c0c81fb63c6d6634aee
chadklebba <chad.klebba@gmail.com>
2018-03-07 07:25:03 -0600

    initial commit

---
+++ Craft/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Craft.Startup))]
+namespace Craft
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
